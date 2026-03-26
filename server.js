84e6c28dd7608a61d6e22939bb7dc67fbba5605e
Fernando Montiel <nando_741@live.com>
2026-03-26 10:40:30 -0500

    added json, modules and created wake up ping

--- server.js
+++ server.js
@@ -10,3 +10,13 @@ const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN || 'http://localhost:5500';
 app.use(cors({ origin: ALLOWED_ORIGIN }));
 app.use(express.json());
 
+// 1. ping (wake-up)
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'Server is awake' });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
